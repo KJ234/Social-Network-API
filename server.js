b07ddef0d04d50477d5a2e1c9ca2b0c1b55629b4
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-05-07 20:48:17 +0100

    update server.js

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require("express");
-const routes = require("./routes");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+app.use(express.urlencoded({ extended: true }));
+
+app.use(require("./routes"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/social-media-api",
-  {}
+  process.env.MONGODB_URI || "mongodb://localhost:27017/social-network-api",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
 );
 
 mongoose.set("debug", true);
 
-app.listen(PORT, () =>
-  console.log(`Social Media API is connected on localhost:${PORT}`)
-);
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
